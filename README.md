c9de5094e486f721b6700e6382841dbf330675e8
termer <termer@termer.net>
2026-01-04 02:03:44 -0500

    Rewrite converter to a simple async generator

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # geofeed2ranges
 
-Library to extract IP ranges from an RFC 8805 geofeed.
+Library to extract IP ranges from an [RFC 8805](https://datatracker.ietf.org/doc/html/rfc8805) geofeed.
+
+# Install
+
+```bash
+npm install geofeed2ranges
+```
+
+# Example
+
+## Read from URL
+
+```javascript
+import { geofeedToRanges } from 'geofeed2ranges'
+
+const httpRes = await fetch(
+	'https://raw.githubusercontent.com/tmobile/tmus-geofeed/main/tmus-geo-ip.txt',
+)
+
+for await (const ln of geofeedToRanges(httpRes.body)) {
+	console.log(ln)
+}
+```
+
+## Read from file
+
+```javascript
+import { createReadStream } from 'node:fs'
+import { geofeedToRanges } from 'geofeed2ranges'
+
+const file = createReadStream('geofeed.csv')
+
+for await (const ln of geofeedToRanges(file)) {
+	console.log(ln)
+}
+```
--- index.mjs
+++ index.mjs
@@ -1,162 +1,87 @@
-import * as fs from 'node:fs'
-import * as readline from 'node:readline'
-import * as https from 'node:https'
-import * as http from 'node:http'
+import * as readline from 'node:readline/promises'
+import { isIP } from 'node:net'
+import { Readable } from 'node:stream'
 
 /**
- * Opens the geofeed at the specified URL.
- * Should be passed to {@link geofeedToRanges}.
- * @param {string | URL} url The URL.
- * @returns {Promise<http.IncomingMessage>} The request body
+ * Takes in a stream containing an RFC 8805 geofeed CSV file and yields CIDR ranges.
+ * Invalid lines are skipped over.
+ * Lines that are bare IPs without subnets are converted to CIDR notation (e.g. `127.0.0.1` becomes `127.0.0.1/32`).
+ * @param {ReadableStream | NodeJS.ReadableStream} input The input stream for the geofeed CSV file.
+ * @returns {AsyncGenerator<string, void, void>}
+ * @yields Each CIDR range.
  */
-export async function openUrlForGeofeed(url) {
-	/** @type {URL} */
-	let srcUrl
-	if (typeof url === 'string') {
-		srcUrl = new URL(url)
-	} else {
-		srcUrl = url
-	}
-
-	/** @type {import('node:http').get | import('node:https').get} */
-	let get
-	if (srcUrl.protocol === 'https:') {
-		get = https.get
-	} else if (srcUrl.protocol === 'http:') {
-		get = http.get
-	} else {
-		throw new URL(`Non-HTTP URL passed to openUrlForGeofeed: "${srcUrl}"`)
-	}
-
-	const srcRes = await new Promise((promRes, promRej) => {
-		get(srcUrl, promRes).on('error', promRej)
-	})
-
-	if (srcRes.statusCode !== 200) {
-		throw new Error(
-			`Made request to get geofeed at "${srcUrl}", but server returned status ${srcRes.statusCode}`,
-		)
-	}
-
-	return srcRes
-}
-
-/**
- * Opens the file at the specified path for reading.
- * Waits for the file to be open before returning the stream.
- * @param {string} path The file path.
- * @returns {Promise<NodeJS.ReadableStream>} The file read stream.
- */
-export async function openFileForReading(path) {
-	return /** @type {Promise<NodeJS.ReadableStream>} */ (
-		new Promise((res, rej) => {
-			const rs = fs.createReadStream(path)
-			rs.once('open', () => res(rs))
-			rs.once('error', rej)
-		})
-	)
-}
-
-/**
- * Opens the file at the specified path for writing.
- * Waits for the file to be open before returning the stream.
- * @param {string} path The file path.
- * @returns {Promise<NodeJS.WritableStream>} The file write stream.
- */
-export async function openFileForWriting(path) {
-	return /** @type {Promise<NodeJS.WritableStream>} */ (
-		new Promise((res, rej) => {
-			const ws = fs.createWriteStream(path)
-			ws.once('open', () => res(ws))
-			ws.once('error', rej)
-		})
-	)
-}
-
-/**
- * Takes in an RFC 8805 geofeed content stream as an input and sends the raw IP/CIDR range lines to the output stream.
- * @param {NodeJS.ReadableStream} input The geofeed content stream.
- * @param {NodeJS.WritableStream & { close(cb?: (err: any) => void): void }} output The output stream to write range lines to.
- * @param {boolean} includeLastUpdated Whether to include a last updated comment at the top of the output.
- * @returns {Promise<void>}
- */
-export async function geofeedToRanges(input, output, includeLastUpdated) {
-	/** @param {string} str */
-	function writeStr(str) {
-		return /** @type {Promise<void>} */ (
-			new Promise((res, rej) => {
-				output.write(str, (err) => {
-					if (err) {
-						rej(err)
-					} else {
-						res()
-					}
-				})
-			})
-		)
-	}
-
-	if (includeLastUpdated) {
-		await writeStr(`# Last updated: ${new Date().toISOString()}\n`)
-	}
-
+export async function* geofeedToRanges(input) {
 	const reader = readline.createInterface({
-		input: input,
-		crlfDelay: Infinity,
-		terminal: false,
-	})
-
-	await /** @type {Promise<void>} */ (
-		new Promise((promRes, promRej) => {
-			let failed = false
-			let lines = 0
-
-			/** @param {any} err */
-			function fail(err) {
-				failed = true
-				output.close()
-				reader.close()
-				promRej(err)
-			}
-
-			input.on('error', fail)
-			output.on('error', fail)
-			reader.on('close', () => {
-				if (failed) {
-					return
-				}
-				if (lines === 0) {
-					fail(
-						new Error(
-							'Found no valid IP range lines from source file',
+		input:
+			input instanceof ReadableStream
+				? Readable.fromWeb(
+						/** @type {import('node:stream/web').ReadableStream} */ (
+							input
 						),
 					)
-					return
-				}
+				: input,
+		crlfDelay: Infinity,
+	})
 
-				reader.close()
-				output.close((err) => {
-					if (err) {
-						fail(err)
-						return
-					}
-					promRes()
-				})
-			})
+	for await (const _ln of reader) {
+		const ln = /** @type {string} */ (_ln)
 
-			reader.on('line', (ln) => {
-				if (ln === '' || ln[0] === '#') {
-					return
-				}
+		if (ln === '') {
+			continue
+		}
+		if (ln[0] === '#') {
+			continue
+		}
 
-				const commaIdx = ln.indexOf(',')
-				if (commaIdx === -1) {
-					return
-				}
+		const commaIdx = ln.indexOf(',')
+		const ipLn = commaIdx === -1 ? ln : ln.substring(0, commaIdx)
 
-				writeStr(ln.slice(0, commaIdx) + '\n').catch(fail)
-				lines++
-			})
-		})
-	)
+		const slashIdx = ipLn.lastIndexOf('/')
+
+		/** @type {string} */
+		let ip
+		/** @type {number} */
+		let subnet
+
+		if (slashIdx === -1) {
+			ip = ipLn
+
+			const ipVer = isIP(ip)
+			if (ipVer === 0) {
+				// Invalid IP string.
+				continue
+			}
+
+			if (ipVer === 6) {
+				subnet = 128
+			} else {
+				subnet = 32
+			}
+		} else {
+			ip = ipLn.substring(0, slashIdx)
+			subnet = parseInt(ipLn.substring(slashIdx + 1))
+
+			if (isNaN(subnet) || subnet < 0) {
+				// Invalid subnet mask.
+				// Probably invalid IP string.
+				continue
+			}
+
+			const ipVer = isIP(ip)
+			if (ipVer === 0) {
+				// Invalid IP string.
+				continue
+			}
+			if (ipVer === 6 && subnet > 128) {
+				// Invalid IPv6 subnet mask.
+				continue
+			}
+			if (ipVer === 4 && subnet > 32) {
+				// Invalid IPv4 subnet mask.
+				continue
+			}
+		}
+
+		yield `${ip}/${subnet}`
+	}
 }
